3224cadfdc34cc283c3a5d5e6d27453f2d66d1d6
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-03-16 20:53:22 +0600

    load data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   const product =[
@@ -11,6 +12,8 @@ function App() {
       {
         product.map(pro => <Product Name={pro.name} price={pro.price}></Product>)
       }
+      <Counter></Counter>
+      <ExternamUser></ExternamUser>
     </div>
   );
 }
@@ -23,6 +26,44 @@ function Product(props){
     </div>
   )
 }
+function Counter(){
+  const[count,setCount]=useState(0)
+  const increaseCount =()=>setCount(count+1)
+  const decreasCount =()=>setCount(count-1)
+  return(
+   <div>
+      <h1>Number:{count}</h1>
+    <button onClick={increaseCount}>Incrices</button>
+    <button onClick={decreasCount}>Decries</button>
+   </div>
+  )
+}
+
+function ExternamUser(){
+  const[user,setUser]=useState()
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/comments")
+    .then(res =>res.json())
+    .then(data => setUser(data))
+  },[])
+  return (
+    <div>
+      <h1>ExternamUser</h1>
+      <p>{user.length}</p>
+      {
+        user.map(use =>)
+      }
+    </div>
+  )
+}
+
+function User(props){
+  return(
+    <div>
+      
+    </div>
+  )
+}
 
 export default App;
-<h1>dsfsdjfh</h1>
+
